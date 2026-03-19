f9bdfaf68f31b0e119efbcfe6524aa34936eede1
Farnaz-01 <farnazfaria12@gmail.com>
2026-03-19 20:24:10 +0600

    at the middle

--- pracDom2.php
+++ pracDom2.php
@@ -3,19 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <title>DOM practice 2</title>
 </head>
 <body>
 
+<div id=f1>
+<div class=box> first div </div>
+<div class=box> second div </div>
+<div class=box>  third div</div>
 
-<div class=box>  </div>
-<div class=box>  </div>
-<div class=box>  </div>
+
+<div class=try> first div </div>
+<div class=try> second div </div>
+<div class=try>  third div</div>
 
 <script>
-let box = document.gebcn("box")
-box.
+//let box = document.gebcn("box")
+let box1 =document.getElementByClassName("box")
 
+box1[0].innerText = "This is the first div"
+box[1].innerHTML = "This is the second div" 
+box[2].innerHTML = "This is the third div"
+</div>
 </script>
 
 
