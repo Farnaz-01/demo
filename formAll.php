4f7d432585183e63000443f39e085ea99c456152
Farnaz-01 <farnazfaria12@gmail.com>
2026-03-26 12:34:52 +0600

    push testing new

---
+++ formAll.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Registration Form</title>
+<link rel="stylesheet" href="styleForm.css">
+</head>
+<body>
+<h1> Registraion Form</h1>
+<form>
+
+<table>
+
+<tr>
+    <td> Name :  </td>
+
+    <td> <input type="text" placeholder="Enter Your Name" minlength=2  </td>
+</tr>
+<tr>
+    <td>   </td>
+
+    <td>   </td>
+</tr>
+<tr>
+    <td>   </td>
+
+    <td>   </td>
+</tr>
+<tr>
+    <td>   </td>
+
+    <td>   </td>
+</tr>
+<tr>
+    <td>   </td>
+
+    <td>   </td>
+</tr>
+<tr>
+    <td>   </td>
+
+    <td>   </td>
+</tr>
+<tr>
+    <td>   </td>
+
+    <td>   </td>
+</tr>
+<tr>
+    <td>   </td>
+
+    <td>   </td>
+</tr>
+<tr>
+    <td>   </td>
+
+    <td>   </td>
+</tr>
+
+
+</table>
+
+
+</form>
+</body>
+</html>
