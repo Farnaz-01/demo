7f71849d4504cf757460c8980a80c814317e2d2e
Farnaz-01 <farnazfaria12@gmail.com>
2026-04-05 00:43:42 +0600

    prac mid 4 css

---
+++ midPrac04.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head><title>
+   Grid Layout
+</title> 
+<link rel="stylesheet" href="grid.css">
+
+
+</head>
+ 
+<body>
+   
+    <div id="kn">
+<div id=per>
+Bottom Right
+</div>
+</div>
+ 
+</body>
+</html>
